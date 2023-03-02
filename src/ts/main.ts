d5f7e41c0f307bf12458785713bd8664bc9593ac
Anna Hrulckova <annahrulkova@yandex.ru>
2023-03-02 18:17:54 +0400

    main

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,12 +1,13 @@
 import "./slider"
 import modals from "./modals"
 import forms from "./forms"
-
+import changeCalkModalState from "./changeCalkModalState"
 
 const calkModalState:{ [key: string]: string } = {}//?  при объявлении внутри функции, возникает ошибка 
 window.addEventListener("DOMContentLoaded", function () {
     modals(),
     forms(calkModalState)
+    changeCalkModalState(calkModalState)
 })
 
  
